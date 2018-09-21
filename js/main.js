96d434f705f3adb36bb7382fa800f06e885b408c
yazaman <yazaman@users.noreply.github.com>
2018-09-22 00:44:27 +0300

    Sorry
    I'm sorry that I'd wasted too much time on challenge 5. I did not find the time for challenge 6.

---
+++ js/main.js
@@ -0,0 +1,22 @@
+console.log('app is alive')
+//switching channels
+function switchChannel () {
+    console.log('Tuning into channel' + 'channelName');
+    document.getElementById().innerHTML='#KillerApp';
+}
+function switchChannel (SevenContinents) {
+    console.log('Tuning into channel' + 'channelName');
+    document.getElementById('SevenContinents').innerHTML='#KillerApp';
+}
+function switchChannel (KillerApp) {
+    console.log('Tuning into channel' + 'channelName');
+    document.getElementById('KillerApp').innerHTML='#Yummy';
+}
+function switchChannel (FirstPersonOnMars) {
+    console.log('Tuning into channel' + 'channelName');
+    document.getElementById('FirstPersonOnMars').innerHTML='#KillerApp';
+}
+function switchChannel (OctoberFest) {
+    console.log('Tuning into channel' + 'channelName');
+    document.getElementById('OctoberFest').innerHTML='#KillerApp';
+}
